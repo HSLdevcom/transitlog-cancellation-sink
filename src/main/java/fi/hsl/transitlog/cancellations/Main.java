c350905ace448797a5d6538939759c7e27d68f7e
Kenta Kusumoto <kenta.kusumoto@futurice.com>
2019-05-09 15:52:59 +0300

    Use ConfigUtils.getConnectionStringFromFileOrThrow

--- src/main/java/fi/hsl/transitlog/cancellations/Main.java
+++ src/main/java/fi/hsl/transitlog/cancellations/Main.java
@@ -8,12 +8,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
-
     private static final Logger log = LoggerFactory.getLogger(Main.class);
-
 
     public static void main(String[] args) {
         log.info("Launching transitlog-cancellation-sink.");
@@ -23,7 +22,7 @@ public class Main {
         log.info("Configuration read, launching the main loop");
         DbWriter writer = null;
         try (PulsarApplication app = PulsarApplication.newInstance(config)) {
-            final String connectionString = readConnectionString();
+            final String connectionString = ConfigUtils.getConnectionStringFromFileOrThrow(Optional.of("/run/secrets/db_conn_string"));
             writer = DbWriter.newInstance(config, connectionString);
             MessageProcessor processor = new MessageProcessor(app, writer);
             log.info("Starting to process messages");
@@ -36,25 +35,5 @@ public class Main {
                 writer.close();
             }
         }
-
-    }
-
-    private static String readConnectionString() throws Exception {
-        String connectionString = "";
-        try {
-            //Default path is what works with Docker out-of-the-box. Override with a local file if needed
-            final String secretFilePath = ConfigUtils.getEnv("FILEPATH_CONNECTION_STRING")
-                    .orElse("/run/secrets/db_conn_string");
-            connectionString = new Scanner(new File(secretFilePath))
-                    .useDelimiter("\\Z").next();
-        } catch (Exception e) {
-            log.error("Failed to read DB connection string from secrets", e);
-            throw e;
-        }
-
-        if (connectionString.isEmpty()) {
-            throw new Exception("Failed to find DB connection string, exiting application");
-        }
-        return connectionString;
     }
 }
