8d6defe7d0a5480d48cd0ae0727de3408760181b
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2023-01-12 16:31:25 +0200

    Fix query

--- src/main/resources/trip_cancellation.sql
+++ src/main/resources/trip_cancellation.sql
@@ -2,4 +2,4 @@ INSERT INTO CANCELLATION
     (deviation_case_id, status, start_date, route_id, direction_id, start_time, last_modified, data, ext_id_dvj)
     VALUES
     (?, ?::CANCELLATION_STATUS, ?, ?, ?, ?, ?, ?::JSON, ?)
-    ON CONFLICT (unique_cancellation_constraint) DO UPDATE SET status = EXCLUDED.status, data = EXCLUDED.data, last_modified = EXCLUDED.last_modified;
+    ON CONFLICT ON CONSTRAINT unique_cancellation_constraint DO UPDATE SET status = EXCLUDED.status, data = EXCLUDED.data, last_modified = EXCLUDED.last_modified;
