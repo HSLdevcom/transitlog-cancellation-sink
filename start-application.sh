3ab6e79ca6d950ead2da95402e6fc5b4799bc157
Anton Jyrkiäinen <anton.jyrkiainen@helsinki.fi>
2023-10-16 14:10:57 +0300

    env update

--- start-application.sh
+++ start-application.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -XX:InitialRAMPercentage=10.0 -XX:MaxRAMPercentage=95.0 -Ddb.username=$(cat /run/secrets/TRANSITLOG_TIMESCALE_USERNAME) -Ddb.password=$(cat /run/secrets/TRANSITLOG_TIMESCALE_PASSWORD) -jar /usr/app/transitlog-cancellation-sink.jar
+java -XX:InitialRAMPercentage=10.0 -XX:MaxRAMPercentage=95.0 -Ddb.username=$TRANSITLOG_TIMESCALE_USERNAME -Ddb.password=$TRANSITLOG_TIMESCALE_PASSWORD -jar /usr/app/transitlog-cancellation-sink.jar
